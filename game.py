1a32549de68bbec42d7e55881c30549ae77d3880
yujiex9 <yujiex9@outlook.com>
2021-02-09 11:53:40 +0000

    Finalise the design by applying SRP

--- game.py
+++ game.py
@@ -1,31 +1,52 @@
 class Game:
     def __init__(self) -> None:
-        self._throws = []
+        self._scorer = Scorer()
         self._current_frame = 0
         self._is_first_throw_in_frame = True
-
-        # internal state
-        self.__throw = 0
 
     def add(self, pins: int):
         self._update_current_frame()
         self._reset_is_first_throw_flag(pins)
-        self._throws.append(pins)
+        self._scorer.add_pins(pins)
 
     def _update_current_frame(self) -> bool:
-        def advance_frame():
+        def _advance_frame():
             self._current_frame = min(10, self._current_frame + 1)
+
         if self._is_first_throw_in_frame:
-            advance_frame()
+            _advance_frame()
 
     def _reset_is_first_throw_flag(self, pins: int):
-        def strike() -> bool:
+        def _is_a_strike_frame() -> bool:
             return pins == 10
+
         if self._is_first_throw_in_frame:
-            if not strike():
+            if not _is_a_strike_frame():
                 self._is_first_throw_in_frame = False
         else:
             self._is_first_throw_in_frame = True
+
+    def score_for_frame(self, frame: int) -> int:
+        return self._scorer.score_for_frame(frame)
+
+    @property
+    def current_frame(self) -> int:
+        return self._current_frame
+
+    @property
+    def score(self) -> int:
+        return self._scorer.score_for_frame(self.current_frame)
+
+
+class Scorer:
+    def __init__(self) -> None:
+        self._throws = []
+
+        # internal state
+        self.__throw = 0
+
+    def add_pins(self, pins: int):
+        self._throws.append(pins)
 
     def score_for_frame(self, frame: int) -> int:
         if len(self._throws) < 2:
@@ -58,11 +79,3 @@ class Game:
 
     def _two_neighbouring_throws(self) -> int:
         return self._throws[self.__throw] + self._throws[self.__throw + 1]
-
-    @property
-    def current_frame(self) -> int:
-        return self._current_frame
-
-    @property
-    def score(self) -> int:
-        return self.score_for_frame(self.current_frame)
